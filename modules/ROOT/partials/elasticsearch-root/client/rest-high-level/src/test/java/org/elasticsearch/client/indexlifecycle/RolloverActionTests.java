469936c071a9746bc78e434a968abe3648acbddf
王斌 <253498229@qq.com>
2022-06-08 10:36:58 +0800

    add document

---
+++ modules/ROOT/partials/elasticsearch-root/client/rest-high-level/src/test/java/org/elasticsearch/client/indexlifecycle/RolloverActionTests.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+package org.elasticsearch.client.indexlifecycle;
+
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.test.AbstractXContentTestCase;
+
+public class RolloverActionTests extends AbstractXContentTestCase<RolloverAction> {
+
+    @Override
+    protected RolloverAction doParseInstance(XContentParser parser) {
+        return RolloverAction.parse(parser);
+    }
+
+    @Override
+    protected boolean supportsUnknownFields() {
+        return true;
+    }
+
+    @Override
+    protected RolloverAction createTestInstance() {
+        return randomInstance();
+    }
+
+    static RolloverAction randomInstance() {
+        ByteSizeUnit maxSizeUnit = randomFrom(ByteSizeUnit.values());
+        ByteSizeValue maxSize = randomBoolean() ? null : new ByteSizeValue(randomNonNegativeLong() / maxSizeUnit.toBytes(1), maxSizeUnit);
+        Long maxDocs = randomBoolean() ? null : randomNonNegativeLong();
+        TimeValue maxAge = (maxDocs == null && maxSize == null || randomBoolean())
+            ? TimeValue.parseTimeValue(randomPositiveTimeValue(), "rollover_action_test")
+            : null;
+        return new RolloverAction(maxSize, maxAge, maxDocs);
+    }
+
+    public void testNoConditions() {
+        IllegalArgumentException exception = expectThrows(IllegalArgumentException.class,
+            () -> new RolloverAction(null, null, null));
+        assertEquals("At least one rollover condition must be set.", exception.getMessage());
+    }
+}
